f0760fdca2dd6b74e2bfad77439ff95d980c7bb5
Roy Macías <roymcsljr@gmail.com>
2023-03-29 00:28:17 -0600

    Se cambió el skybox

--- 02-SkeletonAnimation/src/main.cpp
+++ 02-SkeletonAnimation/src/main.cpp
@@ -96,12 +96,12 @@ GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
 GL_TEXTURE_CUBE_MAP_POSITIVE_Z,
 GL_TEXTURE_CUBE_MAP_NEGATIVE_Z };
 
-std::string fileNames[6] = { "../Textures/mp_bloodvalley/blood-valley_ft.tga",
-		"../Textures/mp_bloodvalley/blood-valley_bk.tga",
-		"../Textures/mp_bloodvalley/blood-valley_up.tga",
-		"../Textures/mp_bloodvalley/blood-valley_dn.tga",
-		"../Textures/mp_bloodvalley/blood-valley_rt.tga",
-		"../Textures/mp_bloodvalley/blood-valley_lf.tga" };
+std::string fileNames[6] = { "../Textures/ForbiddenCity/posx.jpg",
+		"../Textures/ForbiddenCity/negx.jpg",
+		"../Textures/ForbiddenCity/posy.jpg",
+		"../Textures/ForbiddenCity/negy.jpg",
+		"../Textures/ForbiddenCity/posz.jpg",
+		"../Textures/ForbiddenCity/negz.jpg" };
 
 bool exitApp = false;
 int lastMousePosX, offsetX = 0;
